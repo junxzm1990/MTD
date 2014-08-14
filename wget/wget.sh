fe7628136b8b70a94ad72f678284d7eecd30a510
llvm <llvm@client-75-102-96-139.mobile.psu.edu>
2014-08-14 12:28:32 -0700

    add wget

---
+++ wget/wget.sh
@@ -0,0 +1,9 @@
+make clean
+export PATH=/home/llvm/git/whole-program-llvm/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+export LLVM_COMPILER=clang
+CC=wllvm ./configure
+make 
+rm src/wget.bc
+extract-bc src/wget
+rm /var/www/wget.bc
+cp src/wget.bc /var/www
